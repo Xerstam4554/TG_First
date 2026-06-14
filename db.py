8508b21ee96ebd40e2e331115b45c7ce5709873f
Andrey <Andrey4554545@yandex.ru>
2026-06-14 16:35:08 +0300

    Created db.py - file for work with DB

---
+++ db.py
@@ -0,0 +1,14 @@
+import sqlite3
+
+
+def init_db():
+    """Initialize the database"""
+    conn = sqlite3.connect('database.db')
+    cursor = conn.cursor()
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS slots (id INTEGER PRIMARY KEY, date TEXT, time TEXT, is_free True)""")
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS appointments (id INTEGER PRIMARY KEY, user_id INTEGER 
+    name TEXT, phone TEXT, service TEXT, date TEXT, time TEXT, status TEXT)""")
+    conn.commit()
+    conn.close()
